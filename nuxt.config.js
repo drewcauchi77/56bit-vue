e0f0971d56645a3ba768f5a96767f68f7a2b363a
Andrew Cauchi <cauchi1020@gmail.com>
2021-04-06 21:50:54 +0200

    Fix 3

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,4 +58,9 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   },
+
+  server: {
+    port: 8080, // default: 3000
+    host: '0.0.0.0' // default: localhost
+  }
 }
