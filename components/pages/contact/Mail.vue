5b6fa344ac073d94e3e11656eeb8d0329cfd7637
Andrew Cauchi <cauchi1020@gmail.com>
2021-04-11 21:42:37 +0200

    Built the contact us page creating message in API

---
+++ components/pages/contact/Mail.vue
@@ -0,0 +1,6 @@
+<template>
+    <svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M14 1H2C1.44772 1 1 1.44772 1 2V10C1 10.5523 1.44772 11 2 11H14C14.5523 11 15 10.5523 15 10V2C15 1.44772 14.5523 1 14 1Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M14.7738 1.36664L9.3445 5.54264C8.95898 5.83921 8.48623 6.00001 7.99983 6.00001C7.51343 6.00001 7.04068 5.83921 6.65516 5.54264L1.22583 1.36664" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+</template>
--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,11 +1,122 @@
 <template>
-  <div class="container">
-    CONTACT PAGE
-  </div>
+  <main role="main" class="page-content contact-main">
+    <section class="contact-section">
+      <div class="container contact-container">
+        <div class="details-section">
+          <h1 class="details-title">How can we help you?</h1>
+
+          <p class="details-body">56Bit cloud infrastructure experts are more than happy to add value to your business. Please do send us a message below...we’re more than happy to respond.</p>
+        </div>
+
+        <div class="form-container">
+          <div class="form-section" v-if="!isBot">
+            <form v-on:submit.prevent="sendMessage">
+              <p>Dear 56Bit cloud infrastructure experts, <br><br> At&nbsp;
+
+                <label for="company">company</label>
+                <input v-model="company" required name="company" id="company" placeholder="company" />
+
+                we could really do with some expertise relating to &nbsp;
+
+                <label for="query">query</label>
+                <select v-model="query" required name="query" id="query">
+                  <option disabled value="">query</option>
+                  <option>Query A</option>
+                  <option>Query B</option>
+                  <option>Query C</option>
+                </select>
+
+                . <br><br>Please do reach out to me on
+
+                <label for="email">email</label>
+                <input v-model="email" required name="email" id="email" type="email" placeholder="email" />
+
+                . <br><br> I look forward to hearing from you,
+
+                  <label for="full_name">full name</label>
+                  <input v-model="name" required name="name" id="full_name" placeholder="full name" />
+              </p>  
+
+              <div class="hidden">
+                <label>Don’t fill this out if you're human: </label>
+                <input v-model="website" name="website" placeholder="This field is only for the robots." />
+              </div>
+              
+              <div class="submit-button button">
+                <button type="submit">
+                  {{ loading ? "Sending Message..." : "Submit" }}
+                </button>
+              </div>
+
+              <div v-if="success" class="form-message">
+                <p class="success-message">Thank you for contacting us! Your message has been sent successfully.</p>
+              </div>
+              
+              <div v-if="error" class="form-message">
+                <p class="error-message">Bummer, something went wrong. Did you fill out all of the fields?</p>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <div class="contact-details-section">
+          <div class="phone-section contact-sect">
+            <div class="icon">
+              <PagesContactPhone />
+            </div>
+            <span class="phone">(+356) 9978 7661</span>
+          </div>
+
+          <div class="email-section contact-sect">
+            <div class="icon">
+              <PagesContactMail />
+            </div>
+            <span class="email">info@56bit.com</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
 export default {
-  transition: 'slide-bottom'
+  transition: 'slide-bottom',
+  data() {
+    return {
+      api_url: process.env.strapiBaseUri,
+      loading: false,
+      success: false,
+      error: false,
+      isBot: false,
+      company: '',
+      query: '',
+      email: '',
+      name: '',
+      website: null,
+    }
+  },
+  methods: {
+    sendMessage() {
+      this.loading = true
+      if(this.website != null){
+        this.isBot = true;
+      }else{
+        this.$axios.post(this.api_url + "/messages", {
+          Company: this.company,
+          Query: this.query,
+          Email: this.email,
+          FullName: this.name,
+        }).then(response => {
+          this.success = true
+          this.error = false
+        }).catch(error => {
+          this.error = true
+        }).finally(() => {
+          this.loading = false
+        });
+      }
+    }
+  }
 }
 </script>
